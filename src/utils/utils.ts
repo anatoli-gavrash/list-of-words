8279b2a7bdc2da19da17ce68d9df6699bfea342b
Anatoli Gavrash <gaver5@yandex.ru>
2024-02-07 23:23:38 +0300

    Add converter file to blob

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,3 +4,15 @@ export const randomInteger = (min: number = 0, max: number = 1000000000): number
 
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+export const fileToBlob = (file: File): Promise<string | ArrayBuffer | null> => {
+  return new Promise((res, rej) => {
+    const reader = new FileReader();
+    
+    reader.onloadend = () => {
+      res(reader.result);
+    };
+    
+    reader.readAsDataURL(file);
+  });
+};

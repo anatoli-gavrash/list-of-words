d76a9130dadc44c2bb375bf5a179a50538f07b82
Anatoli Gavrash <gaver5@yandex.ru>
2024-02-08 09:18:10 +0300

    Change main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import "./App.css"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { Outlet } from "react-router-dom";
+import { StoreActions } from "./store/store.types";
+import { fetchFirestoreAction } from "./store/actions";
+import { getUserSelector } from "./store/selectors";
+import styles from "./App.module.css";
+import CardList from "./components/card-list";
 
 const App = () => {
+  const dispatch = useDispatch<Dispatch<StoreActions>>();
+  const user = useSelector(getUserSelector);
+
+  useEffect(() => {
+    if (user) dispatch(fetchFirestoreAction({cardsId: user.cardsId}));
+  }, []);
+
   return (
     <>
-      <header>List of Words</header>
-      <main>
-        <aside></aside>
+      <header className={styles.header}></header>
+      <main className={styles.main}>
+        <aside className={styles.aside}>
+          <CardList />
+        </aside>
+        <Outlet />
       </main>
-      <footer></footer>
+      <footer className={styles.footer}></footer>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
